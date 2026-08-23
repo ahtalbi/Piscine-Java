6c39fd601ffcfe13ba66093ea4a1848a71c95e26
ahtalbi <obetox1@gmail.com>
2026-08-23 13:48:05 +0100

    int operations

--- IntOperations/IntOperations.java
+++ IntOperations/IntOperations.java
@@ -2,7 +2,7 @@ public class IntOperations {
 	public static int addTwoIntegers(int a, int b) {
 		return a + b;
 	}
-	public static int substractTwoIntegers(int a, int b) {
+	public static int subtractTwoIntegers(int a, int b) {
 		return a - b;
 	}
 	public static int multiplyTwoIntegers(int a, int b) {
