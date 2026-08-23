9c1222be1e193f722f99664129709c53c26880fa
ahtalbi <obetox1@gmail.com>
2026-08-23 06:05:26 +0100

    string replace

---
+++ StringReplace/ExerciseRunner.java
@@ -0,0 +1,5 @@
+public class ExerciseRunner {
+	public static void main(String[] args) {
+		System.out.println(StringReplace.replace("hello world", 'l', 'a'));
+	}
+}
---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,5 @@
+public class StringReplace {
+	public static String replace(String s, CharSequence target, CharSequence replacment) {
+		return s.replace(target, replacment);
+	}
+}
