5448d0e1c640d8f4a4c886d4b64748c4660da8c5
ahtalbi <obetox1@gmail.com>
2026-08-23 16:41:30 +0100

    compute array

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,9 +1,10 @@
 public class ComputeArray {
 	public static int[] computeArray(int[] arr) {
+		if (arr == null) return null;
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] % 3 == 0) {
 				arr[i] = arr[i] * 5;
-			} else if ((arr[i] - 1) % 3 == 0) {
+			} else if ((arr[i] % 3) - 1 == 0) {
 				arr[i] += 7;
 			}
 		}
