00c5bfd50304e75fb1eb8e62f825b8a5d0ce2b3a
ahtalbi <obetox1@gmail.com>
2026-08-23 06:24:34 +0100

    String Replace

--- StringReplace/ExerciseRunner.java
+++ StringReplace/ExerciseRunner.java
@@ -1,5 +1,6 @@
 public class ExerciseRunner {
 	public static void main(String[] args) {
 		System.out.println(StringReplace.replace("hello world", 'l', 'a'));
+		System.out.println(StringReplace.replace("hello world", "ell", "allo"));
 	}
 }
--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -1,5 +1,9 @@
 public class StringReplace {
-	public static String replace(String s, CharSequence target, CharSequence replacment) {
+	public static String replace(String s, char target, char replacment) {
+		return s.replace(target, replacment);
+	}
+	
+	public static String replace(String s, String target, String replacment) {
 		return s.replace(target, replacment);
 	}
 }
