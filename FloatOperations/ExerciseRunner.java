3d326b7a951e5f84e1a3aa148c9767337377e251
ahtalbi <obetox1@gmail.com>
2026-08-23 03:41:21 +0100

    float operations + isEven

---
+++ FloatOperations/ExerciseRunner.java
@@ -0,0 +1,8 @@
+public class ExerciseRunner {
+	public static void main(String[] args) {
+		System.out.println(FloatOperations.addTwoFloats(2.5f, 2.5f));
+		System.out.println(FloatOperations.addTwoFloats(0.5f, 1.0f));
+		System.out.println(FloatOperations.divideTwoFloats(7.0f, 2.0f));
+		System.out.println(FloatOperations.divideTwoFloats(5.6f, 6.9f));
+	}
+}
