bb2bf7e69035c048fedb2698c92a3a53f032c0f6
ahtalbi <obetox1@gmail.com>
2026-08-23 14:28:26 +0100

    exercise runner

--- StringConcat/ExerciseRunner.java
+++ StringConcat/ExerciseRunner.java
@@ -2,5 +2,6 @@ public class ExerciseRunner {
 	public static void main(String[] args) {
 		System.out.println(StringConcat.concat("hello ", "world"));
 		System.out.println(StringConcat.concat("hello ", "hi"));
+		System.out.println(StringConcat.concat("hello ", null));
 	}
 }
--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,11 @@
 public class StringConcat {
 	public static String concat(String a, String b) {
+		if (a == null) {
+			return b;
+		}
+		if (b == null) {
+			return a;
+		}
 		return a + b;
 	}
 }
