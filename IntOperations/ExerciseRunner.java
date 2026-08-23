55c7cbc76947c5cda050d295e1a0041d32e5fc33
ahtalbi <obetox1@gmail.com>
2026-08-23 13:52:36 +0100

    exercise runner fix

--- IntOperations/ExerciseRunner.java
+++ IntOperations/ExerciseRunner.java
@@ -1,7 +1,7 @@
 public class ExerciseRunner {
         public static void main(String[] args) {
                 System.out.println(IntOperations.addTwoIntegers(5, 1));
-                System.out.println(IntOperations.substractTwoIntegers(5, 1));
+                System.out.println(IntOperations.subtractTwoIntegers(5, 1));
                 System.out.println(IntOperations.multiplyTwoIntegers(5, 2));
                 System.out.println(IntOperations.divideTwoIntegers(6, 2));
         }
