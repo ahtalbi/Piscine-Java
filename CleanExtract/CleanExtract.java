2b092362607f443003785cd97de2829ae0240da9
ahtalbi <obetox1@gmail.com>
2026-08-23 16:01:00 +0100

    clean extract

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -1,5 +1,24 @@
 public class CleanExtract {
 	public static String extract(String s) {
-		return ;
+		String[] arr = s.split("\\|");
+		int i = 0;
+		for (String str : arr) {
+			int fi = str.indexOf('.');
+			int li = str.lastIndexOf('.');
+
+			if (fi < 0 && li < 0) {i++; continue;};
+			if (fi == li) {
+				fi += 1;
+				arr[i] = str.substring(fi);
+			} else if (fi < li) {
+				fi += 1;
+				arr[i] = str.substring(fi, li);
+			}
+			i++;
+		}
+		arr = java.util.Arrays.stream(arr).filter(str -> !str.isBlank()).toArray(String[]::new);
+		String res = String.join(" ", arr);
+		res = res.replaceAll("\\s+", "\s");
+		return res;
 	}
 }
