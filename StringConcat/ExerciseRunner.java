b8e2795852a966fa604b96de9eb2a2afb94167fd
ahtalbi <obetox1@gmail.com>
2026-08-23 05:36:01 +0100

    String Concat

---
+++ StringConcat/ExerciseRunner.java
@@ -0,0 +1,6 @@
+public class ExerciseRunner {
+	public static void main(String[] args) {
+		System.out.println(StringConcat.concat("hello ", "world"));
+		System.out.println(StringConcat.concat("hello ", "hi"));
+	}
+}
