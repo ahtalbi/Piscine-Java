670fd5d71edfd67ab37e0e8c3098a7de8e0b6718
ahtalbi <obetox1@gmail.com>
2026-08-25 07:40:00 +0100

    file search

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,21 @@
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileSearch {
+	public static String searchFile(String fileName) {
+		try {
+			List<String> paths = Files.walk(Paths.get("documents"), Integer.MAX_VALUE).map(path -> path.toString()).collect(Collectors.toList());
+			for (String path : paths) {
+				if (path.endsWith(fileName)) {
+					return path;
+				}
+			}
+		} catch (Exception e) {
+			return "";
+		}
+
+		return "";
+	}
+}
