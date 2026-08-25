4ca9f30fe21c289547183f2bf5fa8034559b611b
ahtalbi <obetox1@gmail.com>
2026-08-25 01:26:48 +0100

    DoOp

---
+++ DoOp/DoOp.java
@@ -0,0 +1,28 @@
+public class DoOp {
+	public static String operate(String[] args) {
+		if (args == null || args.length != 3) {
+			return "it depend on the input.";
+		}
+
+		try {
+			int fd = Integer.parseInt(args[0]);	
+			int sd = Integer.parseInt(args[2]);
+			switch (args[1]) {
+				case "+":
+					return String.format("%d", fd + sd);
+				case "-":
+					return String.format("%d", fd - sd);
+				case "*":
+					return String.format("%d", fd * sd);
+				case "/":
+					return String.format("%d", fd / sd);
+				case "%":
+					return String.format("%d", fd % sd);
+				default:
+					return "Error";
+			}
+		} catch (ArithmeticException | NumberFormatException e) {
+			return "Error";
+		}
+	}
+}
