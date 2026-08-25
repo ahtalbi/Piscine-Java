f8e995b21d7ba277aa23aa4f2155f621f6f9835d
ahtalbi <obetox1@gmail.com>
2026-08-25 16:01:31 +0100

    Planet

---
+++ StarPlanet/Planet.java
@@ -0,0 +1,40 @@
+public class Planet extends CelestialObject {
+	private Star centerStar;
+
+	public Planet() {
+		super();
+		this.centerStar = new Star();
+	}
+
+	public Planet(String name, double x, double y, double z, Star centerStar) {
+		super(name, x, y, z);
+		this.centerStar = centerStar;
+	}
+
+	@Override
+	public int hashCode() {
+		return java.util.Objects.hash(super.hashCode(), this.centerStar);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Planet)) {
+			return false;
+		}
+		Planet objCasted = (Planet) obj;
+		return super.equals(objCasted) && this.centerStar.equals(objCasted.getCenterStar());
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s orbits %s", getName(), this.centerStar.getName());
+	}
+
+	public Star getCenterStar() {
+		return this.centerStar;
+	}
+
+	public void setCenterStar(Star centerStar) {
+		this.centerStar = centerStar;
+	}
+}
