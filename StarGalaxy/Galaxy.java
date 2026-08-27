fd9749720544231aa32a041046ef48d80787e76b
ahtalbi <obetox1@gmail.com>
2026-08-27 14:26:09 +0100

    star galaxy

--- StarGalaxy/Galaxy.java
+++ StarGalaxy/Galaxy.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Galaxy {
 	private List<CelestialObject> celestialObjects;
 
-	Galaxy() {
+	public Galaxy() {
 		this.celestialObjects = new ArrayList<>();
 	}
 
