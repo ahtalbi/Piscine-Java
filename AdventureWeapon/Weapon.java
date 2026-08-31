7235c8b9b45f6f62e9a65a4dcacf17c0bcdcbb96
ahtalbi <obetox@gmail.com>
2026-08-31 06:31:42 +0100

    adventure Weapon

--- AdventureWeapon/Weapon.java
+++ AdventureWeapon/Weapon.java
@@ -1,6 +1,6 @@
 public class Weapon {
-	private String name;
-	private int damage;
+	final private String name;
+	final private int damage;
 
 	public Weapon(String name, int damage) {
 		this.name = name;
