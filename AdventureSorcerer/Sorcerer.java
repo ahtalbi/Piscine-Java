5f001d9e0e6eb4008f33beb20fe8fb6ebba6a3cb
ahtalbi <obetox@gmail.com>
2026-08-31 04:00:35 +0100

    adventure sorcerer

---
+++ AdventureSorcerer/Sorcerer.java
@@ -0,0 +1,34 @@
+public class Sorcerer extends Character implements Healer {
+	private int healCapacity;
+
+	// Getters
+	public int getHealCapacity() {
+		return this.healCapacity;
+	}
+
+	// Setters
+	public void setHealCapacity(int hc) {
+		this.healCapacity = hc;
+	}
+
+	public Sorcerer(String name, int maxHealth, int healCapacity) {
+		super(name, maxHealth);
+		this.healCapacity = healCapacity;
+	}
+
+	public void heal(Character c) {
+		if (this.healCapacity + c.getCurrentHealth() > c.getMaxHealth()) {
+			c.setCurrentHealth(c.getMaxHealth());
+			return;
+		}
+		c.setCurrentHealth(c.getCurrentHealth()+this.healCapacity);
+	}
+
+	@Override
+	public String toString() {
+		if (this.getCurrentHealth() != 0) {
+			return String.format("%s is a sorcerer with %d HP. It can heal %d HP.", this.getName(), this.getCurrentHealth(), this.healCapacity);
+		}
+		return String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", this.getName(), this.healCapacity);
+	}
+}
