2b8a3b9e615d7d5f6948b2ccf1c4f7fbdb897e70
ahtalbi <obetox1@gmail.com>
2026-09-04 21:50:45 +0100

    list sorted

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -4,12 +4,12 @@ import java.util.Comparator;
 
 public class SortList {
 	public static List<Integer> sort(List<Integer> list) {
-	    if (list == null) return new ArrayList<>();
+	    if (list == null) return null;
 	    return list.stream().sorted().toList(); 
 	}
 	
 	public static List<Integer> sortReverse(List<Integer> list) {
-	    if (list == null) return new ArrayList<>();
+	    if (list == null) return null;
 	    return list.stream().sorted(Comparator.reverseOrder()).toList();
 	}
 }
