5e3e5c117df4d683b11491306b574a7a8dde9523
ahtalbi <obetox1@gmail.com>
2026-09-04 21:37:00 +0100

    list search fix

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -1,20 +1,23 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
         if (list == null) return null;
-        return list.lastIndexOf(value);
+	int ind = list.lastIndexOf(value);
+        return (ind == -1) ? null : ind;
     }
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
         if (list == null) return null;
-        return list.indexOf(value);
+	int ind = list.indexOf(value);
+        return (ind == -1) ? null : ind;
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
         List<Integer> res = new ArrayList<>();
         if (list == null) return res;
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == value) {
+            if (Objects.equals(list.get(i), value)) {
                 res.add(i);
             }
         }
