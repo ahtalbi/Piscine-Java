74e41f7533d42ac2eb0a860ea6c074d115a3e18d
ahtalbi <obetox1@gmail.com>
2026-09-05 06:17:42 +0100

    finish

--- WeddingComplex/WeddingComplex.java
+++ WeddingComplex/WeddingComplex.java
@@ -3,38 +3,56 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class WeddingComplex {
-    public static Map<String, String> createBestCouple(Map<String, List<String>> mans, Map<String, List<String>> womans) {
-	int size = (mans.size() > womans.size()) ? womans.size() : mans.size();
-	Map<String, String>  res = new HashMap<>();
-	Map<String, Integer> mariages = new HashMap<>();
+    public static Map<String, String> createBestCouple( Map<String, List<String>> mans, Map<String, List<String>> womans) {
+        int size = (mans.size() > womans.size()) ? womans.size() : mans.size();
 
-	for (Map.Entry<String, List<String>> man: mans.entrySet()) {
-		String mkey = man.getKey();
-		for (String woman : man.getValue()) {
-			int score = 0;
-			List<String> womanList = womans.get(woman);
-		       	List<String> manList = mans.get(mkey);
-	
-			score = score + womanList.indexOf(mkey);
-			score = score + manList.indexOf(woman);
+        Map<String, String> res = new HashMap<>();
+        Map<String, String> mariages = new HashMap<>();
+        Map<String, Integer> nextProposal = new HashMap<>();
 
-			String keyMar = mkey + "-" + woman;
-			mariages.put(keyMar, score);			
-		}
-	}
+        for (String man : mans.keySet()) {
+            nextProposal.put(man, 0);
+        }
 
-	List<String> mariagesList = mariages.entrySet().stream().sorted(Map.Entry.comparingByValue()).map(Map.Entry::getKey).toList();
-	int i = 0;
-	while (res.size() != size) {
-		String[] mariage = mariagesList.get(i).split("-");
-		String man = mariage[0];
-		String woman = mariage[1];
-		if (!res.containsKey(man)) {
-			res.put(man, woman);
-		}
-		i++;
-	}
+        while (res.size() < size) {
 
-	return res;
+            for (Map.Entry<String, List<String>> man : mans.entrySet()) {
+                String mkey = man.getKey();
+                if (res.containsKey(mkey)) {
+                    continue;
+                }
+
+                List<String> preferences = man.getValue();
+                int index = nextProposal.get(mkey);
+
+                if (index >= preferences.size()) {
+                    continue;
+                }
+
+                String woman = preferences.get(index);
+                nextProposal.put(mkey, index + 1);
+                if (!mariages.containsKey(woman)) {
+                    mariages.put(woman, mkey);
+                    res.put(mkey, woman);
+                    continue;
+                }
+
+                String oldMan = mariages.get(woman);
+                List<String> womanPreferences = womans.get(woman);
+
+                int newManRank = womanPreferences.indexOf(mkey);
+                int oldManRank = womanPreferences.indexOf(oldMan);
+
+                if (newManRank < oldManRank) {
+
+                    mariages.put(woman, mkey);
+
+                    res.remove(oldMan);
+                    res.put(mkey, woman);
+                }
+            }
+        }
+
+        return res;
     }
 }
