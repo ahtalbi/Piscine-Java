e2ab33fec8e982cb68a92885bcb0a76312482d49
ahtalbi <obetox1@gmail.com>
2026-09-07 01:44:14 +0100

    fix my regex

--- RegexMatch/RegexMatch.java
+++ RegexMatch/RegexMatch.java
@@ -4,14 +4,14 @@ import java.util.regex.Pattern;
 public class RegexMatch {
     public static boolean containsOnlyAlpha(String s) {
         if (s == null) return false;
-        Pattern pattern = Pattern.compile("^\\w+$");
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
         Matcher matcher = pattern.matcher(s);
         return matcher.find();
     }
     
     public static boolean startWithLetterAndEndWithNumber(String s) {
         if (s == null) return false;
-        Pattern pattern = Pattern.compile("^\\w.+[0-9]$");
+        Pattern pattern = Pattern.compile("^[a-zA-Z].+[0-9]$");
         Matcher matcher = pattern.matcher(s);
         return matcher.find();
     }
